d5bc8a45150a94a54d0fdc8ef0f5de730ffd68da
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-30 21:37:06 -0500

    Populate employees state with random user data, EmployeeTable

--- src/EmployeeTable.js
+++ src/EmployeeTable.js
@@ -1,9 +1,18 @@
 import React from "react";
+import API from "./utils/API.js";
 
 class EmployeeTable extends React.Component {
   state = {
     employees: [],
   };
+
+  componentDidMount() {
+    API.getEmployees().then(employees => {
+      this.setState({ employees }, function () {
+        console.log(this.state.employees);
+      });
+    });
+  }
 
   render() {
     return <h1>Employee Table</h1>;
