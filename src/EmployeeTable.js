d7da04ba61d23fa621970602990327527a7938e5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-30 22:32:23 -0500

    Display table of employee data

--- src/EmployeeTable.js
+++ src/EmployeeTable.js
@@ -15,7 +15,45 @@ class EmployeeTable extends React.Component {
   }
 
   render() {
-    return <h1>Employee Table</h1>;
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1 className="py-3">Employee Table</h1>
+            <table className="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Image</th>
+                  <th scope="col">First Name</th>
+                  <th scope="col">Last Name</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Phone Number</th>
+                  <th scope="col">Postal Code</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.employees.map((employee, index) => (
+                  <tr key={index}>
+                    <td>
+                      <img
+                        src={employee.picture.thumbnail}
+                        alt={`${employee.name.first} ${employee.name.last}`}
+                        style={{ borderRadius: "50%" }}
+                      />
+                    </td>
+                    <td>{employee.name.first}</td>
+                    <td>{employee.name.last}</td>
+                    <td>{employee.email}</td>
+                    <td>{employee.phone}</td>
+                    <td>{employee.location.postcode}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
