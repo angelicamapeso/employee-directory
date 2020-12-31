fa87766ca5e4699b8469a5180772caf2ed4f4345
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-30 22:50:36 -0500

    Limit nationality to CA

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 const API = {
   getEmployees() {
     return fetch(
-      "https://randomuser.me/api/?results=50&exc=login,gender,registered,nat,id&noinfo"
+      "https://randomuser.me/api/?results=50&nat=ca&exc=login,gender,registered,nat,id&noinfo"
     )
       .then(response => response.json())
       .then(obj => obj.results);
