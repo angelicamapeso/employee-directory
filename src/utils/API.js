221d9cef39e8bb9c3a8f178f99909d64116d538c
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-12-30 21:52:57 -0500

    Remove national in API results

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 const API = {
   getEmployees() {
     return fetch(
-      "https://randomuser.me/api/?results=50&exc=login,gender,registered,nat&noinfo"
+      "https://randomuser.me/api/?results=50&exc=login,gender,registered,nat,id&noinfo"
     )
       .then(response => response.json())
       .then(obj => obj.results);
