c496589de7504ee65ea2ff53a2052d43ff27647b
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-01 15:26:34 -0500

    Add list of employee titles, filter

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,3 +1,14 @@
+export const EMPLOYEE_TITLES = [
+  "Web Designer",
+  "Front-End Developer",
+  "Back-End Developer",
+  "Full-Stack Developer",
+  "Dev Ops",
+  "UI Designer",
+  "UX Designer",
+  "Interaction Designer",
+];
+
 /* Each of the filter functions
 takes an array (assumped employee array) and returns
 a filtered array */
