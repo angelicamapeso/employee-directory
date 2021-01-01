e6256901b747575ff8c0d176c80ee93c98890a6a
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-01 15:27:01 -0500

    Add titles to API data

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,20 @@
+import { getRandomTitle } from "./filter";
+
 const API = {
   getEmployees() {
     return fetch(
       "https://randomuser.me/api/?results=50&nat=ca&exc=login,gender,registered,nat,id&noinfo"
     )
       .then(response => response.json())
-      .then(obj => obj.results);
+      .then(obj => addEmployeeTitles(obj.results));
   },
 };
+
+function addEmployeeTitles(userList) {
+  return userList.map(user => {
+    user.title = getRandomTitle();
+    return user;
+  });
+}
 
 export default API;
