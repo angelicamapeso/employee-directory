218a99a2a29adec318a80ee4d8db285bc9b0ef30
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-01 14:28:34 -0500

    Fix sort ignoring sortCol for empB

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -5,7 +5,7 @@ export function sortName(employeeList, sortCol, dir) {
   const employeeListCopy = [...employeeList];
   return employeeListCopy.sort(
     (empA, empB) =>
-      empA.name[sortCol].localeCompare(empB.name.first) *
+      empA.name[sortCol].localeCompare(empB.name[sortCol]) *
       (dir === "sort-up" ? 1 : dir === "sort-down" ? -1 : 0)
   );
 }
