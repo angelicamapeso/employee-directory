85a0217d8b5af594ec5a91468461e9f3c7ac9a98
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-02 18:19:07 -0500

    Center employee row content

--- src/EmployeeRow.js
+++ src/EmployeeRow.js
@@ -6,29 +6,29 @@ import TextMatch from "./TextMatch";
 function EmployeeRow(props) {
   return (
     <tr>
-      <td>
+      <td className="align-middle">
         <img
           src={props.employee.picture.thumbnail}
           alt={`${props.employee.name.first} ${props.employee.name.last}`}
           style={{ borderRadius: "50%" }}
         />
       </td>
-      <td>{props.employee.title}</td>
-      <td>
+      <td className="align-middle">{props.employee.title}</td>
+      <td className="align-middle">
         <TextMatch
           text={props.employee.name.first}
           matchWith={props.nameMatch}
         />
       </td>
-      <td>
+      <td className="align-middle">
         <TextMatch
           text={props.employee.name.last}
           matchWith={props.nameMatch}
         />
       </td>
-      <td>{props.employee.email}</td>
-      <td>{props.employee.phone}</td>
-      <td>{props.employee.location.postcode}</td>
+      <td className="align-middle">{props.employee.email}</td>
+      <td className="align-middle">{props.employee.phone}</td>
+      <td className="align-middle">{props.employee.location.postcode}</td>
     </tr>
   );
 }
